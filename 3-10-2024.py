daa7bf7ee6ad12039ad197c19ced297d507f364e
chinnareddy8374 <chinnareddy1328@gmail.com>
2024-10-04 07:13:06 +0530

    intial commit

---
+++ 3-10-2024.py
@@ -0,0 +1,31 @@
+s="learning python is very easy"
+
+word=s.split()
+
+reversed_sentence=word[4]+' '+word[3]+' '+word[2]+' '+word[1]+' '+word[0]+' '
+
+print(reversed_sentence)
+ 
+s="learning python is very easy"
+
+print(s[::-1])
+
+ 
+s="learning python is very easy"
+
+print(s.replace(" ", ""))
+ 
+s="learning"
+
+print(s.count("l"))
+
+print(s.count("e"))
+
+print(s.count("a"))
+
+print(s.count("r"))
+print(s.count("n"))
+
+print(s.count("i"))
+
+print(s.count("g"))
